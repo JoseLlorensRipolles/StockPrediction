3a6e242c52f577da3aa3283007a4b9aa850b172f
Jose Manuel Llorens <jose.llorens.ripolles@gmail.com>
2020-10-04 10:36:43 +0200

    Black formatting

--- src/StockDataset.py
+++ src/StockDataset.py
@@ -10,17 +10,21 @@ class StockDataset(torch.utils.data.Dataset):
     LOOKBACK = 20
 
     def __init__(self, ticker, train=True):
-        full_data_frame = pd.read_csv('resources/stocks/{ticker}.us.txt'.format(ticker=ticker), index_col=0, parse_dates=True)
-        price = full_data_frame[['Close']]
+        full_data_frame = pd.read_csv(
+            "resources/stocks/{ticker}.us.txt".format(ticker=ticker),
+            index_col=0,
+            parse_dates=True,
+        )
+        price = full_data_frame[["Close"]]
         scaler = MinMaxScaler(feature_range=(-1, 1))
-        price['Close'] = scaler.fit_transform(price['Close'].values.reshape(-1,1))
+        price["Close"] = scaler.fit_transform(price["Close"].values.reshape(-1, 1))
         data_raw = price.to_numpy()
         data = sliding_windows(data_raw, self.LOOKBACK)
 
         idx, length = get_split_idx_and_length(train, data, self.TRAIN_SPLIT)
 
-        self.inputs = torch.FloatTensor(data[idx:idx + length,:-1, :]).to('cuda')
-        self.targets = torch.FloatTensor(data[idx:idx + length, -1, :]).to('cuda')
+        self.inputs = torch.FloatTensor(data[idx : idx + length, :-1, :]).to("cuda")
+        self.targets = torch.FloatTensor(data[idx : idx + length, -1, :]).to("cuda")
 
     def __len__(self):
         return self.inputs.shape[0]
@@ -28,28 +32,28 @@ class StockDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         sample_input = self.inputs[idx]
         sample_target = self.targets[idx]
-        return {'input': sample_input, 'target': sample_target}
+        return {"input": sample_input, "target": sample_target}
 
 
 def get_split_idx_and_length(train, data, train_split):
     if train:
         idx = 0
-        length = int(np.round(train_split*data.shape[0]))
+        length = int(np.round(train_split * data.shape[0]))
     else:
-        idx = int(np.round(train_split*data.shape[0]))
-        length = data.shape[0] - int(np.round(train_split*data.shape[0]))
+        idx = int(np.round(train_split * data.shape[0]))
+        length = data.shape[0] - int(np.round(train_split * data.shape[0]))
     return idx, length
 
 
 def sliding_windows(data, lookback):
     windows = []
-    for index in range(len(data) - (lookback - 1)): 
-        windows.append(data[index: index + lookback])
+    for index in range(len(data) - (lookback - 1)):
+        windows.append(data[index : index + lookback])
     windows = np.array(windows)
     return windows
 
 
 if __name__ == "__main__":
-    dataset = StockDataset('msft')
-    print(dataset.__getitem__(1)['input'])
-    print(dataset.__getitem__(1)['target'])
+    dataset = StockDataset("msft")
+    print(dataset.__getitem__(1)["input"])
+    print(dataset.__getitem__(1)["target"])
--- src/main.py
+++ src/main.py
@@ -10,33 +10,33 @@ import matplotlib.pyplot as plt
 def train():
     num_epochs = 100
 
-    model = GRU(input_dim=1, hidden_dim=500, output_dim=1, num_layers=3).to('cuda')
-    criterion = torch.nn.MSELoss(reduction='mean')
+    model = GRU(input_dim=1, hidden_dim=500, output_dim=1, num_layers=3).to("cuda")
+    criterion = torch.nn.MSELoss(reduction="mean")
     optimiser = torch.optim.Adam(model.parameters(), lr=0.001)
-    dataset = StockDataset(ticker='msft', train=True)
+    dataset = StockDataset(ticker="msft", train=True)
     dataloader = DataLoader(dataset, batch_size=1000, shuffle=True)
 
     hist = []
 
     for t in range(num_epochs):
         for samples in dataloader:
-            x_train = samples['input']
-            y_train = samples['target']
-            y_train_pred = model(x_train)    
+            x_train = samples["input"]
+            y_train = samples["target"]
+            y_train_pred = model(x_train)
             loss = criterion(y_train_pred, y_train)
             print("Epoch ", t, "MSE: ", loss.item())
             hist.append(loss.item())
             optimiser.zero_grad()
             loss.backward()
             optimiser.step()
-    
-    torch.save(model.state_dict(), 'resources/models/model.pt')
+
+    torch.save(model.state_dict(), "resources/models/model.pt")
 
 
 def test_visualization(train=True):
-    model = GRU(input_dim=1, hidden_dim=500, output_dim=1, num_layers=3).to('cuda')
-    model.load_state_dict(torch.load('resources/models/model.pt'))
-    dataset = StockDataset(ticker='msft', train=train)
+    model = GRU(input_dim=1, hidden_dim=500, output_dim=1, num_layers=3).to("cuda")
+    model.load_state_dict(torch.load("resources/models/model.pt"))
+    dataset = StockDataset(ticker="msft", train=train)
     dataloader = DataLoader(dataset, batch_size=1000, shuffle=False)
 
     outputs = np.array([])
@@ -44,13 +44,12 @@ def test_visualization(train=True):
 
     model.eval()
     for samples in dataloader:
-        x_train = samples['input']
-        y_train = samples['target']
-        y_train_pred = model(x_train)    
+        x_train = samples["input"]
+        y_train = samples["target"]
+        y_train_pred = model(x_train)
 
         outputs = np.hstack((outputs, y_train_pred.cpu().detach().numpy().flatten()))
-        targets = np.hstack((targets , y_train.cpu().detach().numpy().flatten()))
-        
+        targets = np.hstack((targets, y_train.cpu().detach().numpy().flatten()))
 
     plt.plot(targets)
     plt.plot(outputs)
